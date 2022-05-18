b760c4e417d2ccc7a04fba36be4670be673d3ddf
Nikola Petrov <73067824+Petrov22Nikola@users.noreply.github.com>
2022-05-18 16:31:38 -0400

    Created basic structure for VirtualSimulation

--- prosthetic.py
+++ prosthetic.py
@@ -1,13 +1,36 @@
 # Imported modules
 import pygame
 import pymunk
+# Locally written modules
+import body
+
+# Constants
+displayWidth = 500
+displayHeight = 500
 
 # Virtual simulation of prosthetic and environment
 class VirtualSimulation:
-    # Render simulation
-    def renderSimulation():
+    # Variables
+    objects = []
+
+    # Methods
+    # Constructor
+    def __init__(self):
         pygame.display.init()
+        
+    # Render simulation
+    def renderSimulation(self):
+        pygame.display.set_mode(size=(displayWidth, displayHeight), flags=0, depth=0, display=0, vsync=0)
+        for obj in self.objects:
+            drawObject(obj)
+            
+    # Draw object
+    def drawObject(self):
+        print("Body")
 
-vs = VirtualSimulation()
-vs.renderSimulation()
+    # Create new object
+    def createObject(self):
+        print("Create")
 
+virtualSimulation = VirtualSimulation()
+virtualSimulation.renderSimulation()
