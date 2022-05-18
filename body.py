92ca54204a8c7a1f2e62a74c0c54ff0e9a5517bf
Nikola Petrov <73067824+Petrov22Nikola@users.noreply.github.com>
2022-05-18 16:32:21 -0400

    Basic structure for body class

---
+++ body.py
@@ -0,0 +1,19 @@
+class Body:
+    class Torso:
+        def __init__(self):
+            print("Torso")
+    class Head:
+        def __init__(self):
+            print("Head")
+    class Arm:
+        def __init__(self):
+            print("Arm")
+        class Finger:
+            def __init__(self):
+                print("Finger")
+    class Leg:
+        def __init__(self):
+            print("Leg")
+        class Toe:
+            def __init__(self):
+                print("Toe")
