bf7561a2012d4e2e9eaa26762df45f3c2f7a8f98
s  s <105819720+HaoYingSun@users.noreply.github.com>
2022-05-18 22:05:28 -0400

    Update PoseModule.py

--- PoseModule.py
+++ PoseModule.py
@@ -38,25 +38,25 @@ class PoseDetector:
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
         return lmList
-
+   
 def main():
     cap = cv2.VideoCapture('videos/a.mp4')
     pTime = 0
-    detector = PoseDetector()
+    detector = Posedetector()
     while True:
         success, img = cap.read()
         img = detector.findPose(img)
         lmList = detector.getPosition(img)
         print(lmList)
-
+        
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
-
-        cv2.putText(img, str(int(fps)), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+        
+        cv2.putText(img, str(int(fps)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (225, 0, 0), 3)
         cv2.imshow("Image", img)
         cv2.waitKey(1)
-
-
+        
+        
 if __name__ == "__main__":
     main()
